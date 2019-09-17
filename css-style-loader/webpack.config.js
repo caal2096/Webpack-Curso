5320578738f61806a81aa44d7ae4b2455c595454
Cesararmas96 <caal2096@gmail.com>
2019-09-16 21:30:25 -0400

    clase9-Css-Style-Loader

---
+++ css-style-loader/webpack.config.js
@@ -0,0 +1,32 @@
+// Un modulo de node y se encarga de la gestion de rutas
+
+const path = require("path");
+// Para exportar un modulo, se debe hacer de la forma commonJS
+
+module.exports = {
+	// El archivo que quiere transpilar
+
+	entry: path.resolve(__dirname, "src/js/index.js"),
+
+	// El modo de ejecucion
+
+	mode: "development",
+	// Configuraciones para el archivo generado por webpack
+
+	output: {
+		// La donde quedara el archivo final
+
+		path: path.resolve(__dirname, "dist"),
+		//El nombre del archivo final creado
+
+		filename: "js/[name].js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"]
+			}
+		]
+	}
+};
