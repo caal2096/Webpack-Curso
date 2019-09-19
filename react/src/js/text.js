8ba68f17443089dc8e7bc58a5fa6c53a2846092e
Cesararmas96 <caal2096@gmail.com>
2019-09-19 10:51:38 -0400

    12-React

---
+++ react/src/js/text.js
@@ -0,0 +1,5 @@
+const text = () => {
+	document.body.innerHTML = "<p> Un pedacito a cada nena </p>";
+};
+
+export default text;
---
+++ react/webpack.config.js
@@ -0,0 +1,58 @@
+// Un modulo de node y se encarga de la gestion de rutas
+
+const path = require("path");
+
+// Para importar Plugin
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Webpack = require("webpack");
+// Para exportar un modulo, se debe hacer de la forma commonJS
+
+module.exports = {
+	// El archivo que quiere transpilar
+
+	entry: path.resolve(__dirname, "src/js/home.js"),
+
+	// El modo de ejecucion
+
+	mode: "development",
+	// Configuraciones para el archivo generado por webpack
+
+	output: {
+		// La donde quedara el archivo final
+
+		path: path.resolve(__dirname, "dist"),
+		//El nombre del archivo final creado
+
+		filename: "js/[name].js"
+	},
+
+	devServer: {
+		hot: true,
+		open: true,
+		port: 9000
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/i,
+				use: "babel-loader",
+				exclude: /node_modules/
+			},
+
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"]
+			}
+		]
+	},
+
+	plugins: [
+		new Webpack.HotModuleReplacementPlugin(),
+
+		new HtmlWebpackPlugin({
+			title: "babel",
+			template: path.resolve(__dirname, "index.html")
+		})
+	]
+};
